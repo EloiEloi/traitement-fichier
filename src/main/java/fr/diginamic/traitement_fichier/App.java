0a71e5e5795e4892edda78b0317acba7a5729a60
Eloi Turpin <eloi.turpin@gmail.com>
2019-05-23 23:02:14 +0200

    modif

--- src/main/java/fr/diginamic/traitement_fichier/App.java
+++ src/main/java/fr/diginamic/traitement_fichier/App.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.Timer;
@@ -13,8 +12,6 @@ import java.util.TimerTask;
 import org.apache.commons.io.FileUtils;
 
 import fr.diginamic.traitement_fichier.connection.ConnectionManager;
-import fr.diginamic.traitement_fichier.dao.CategorieDao;
-import fr.diginamic.traitement_fichier.dao.MarqueDao;
 import fr.diginamic.traitement_fichier.exception.TechnicalException;
 import fr.diginamic.traitement_fichier.model.Categorie;
 import fr.diginamic.traitement_fichier.model.Marque;
@@ -36,7 +33,7 @@ public class App {
 	public static void main(String[] args) {
 
 		long start = System.currentTimeMillis();
-		System.out.println("------------ DEBUT du traitement ------------");
+		System.out.println("------------ DEBUT du traitement du fichier ------------");
 
 		// timer afin d'afficher chaque seconde le temps écoulé (dans la console)
 		TimerTask task = new TimerTask() {
@@ -54,8 +51,10 @@ public class App {
 
 		ArrayList<Produit> listeProduits = new ArrayList<Produit>();
 
+		// hashSet contenant la liste de toute les categories existantes
 		Set<Categorie> listeCategories = new HashSet<>();
 
+		// hashSet contenant la liste de toutes les marques existantes
 		Set<Marque> listeMarques = new HashSet<>();
 
 		// récupération du fichier à lire
@@ -67,6 +66,7 @@ public class App {
 			// on supprime la 1ere ligne afin de ne pas récupérer la 1ère ligne du fichier qui correspond aux noms des colonnes
 			listeLignes.remove(0);
 
+			// boucle pour parcourir la list contenant ligne par ligne le contenu du fichier
 			for (String ligne : listeLignes) {
 
 				// on récupere les categories
@@ -76,33 +76,34 @@ public class App {
 				// on récupere les marques
 				Marque marque = new Marque(ligne);
 				listeMarques.add(marque);
-			}
-
-			// enregistrement dans la BDD du contenu de la liste de Categories
-			CategorieDao catDao = new CategorieDao();
-			Iterator<Categorie> monIt = listeCategories.iterator();
-			while (monIt.hasNext()) {
-				Categorie maCat = monIt.next();
-				catDao.insert(maCat);
-				// System.out.println(maCat.getNom());
-			}
-
-			// enregistrement dans la BDD du contenu de la liste de Marques
-			MarqueDao marDao = new MarqueDao();
-			Iterator<Marque> monIt2 = listeMarques.iterator();
-			while (monIt.hasNext()) {
-				Marque maMar = monIt2.next();
-				marDao.insert(maMar);
-
-				// System.out.println(maMar.getNom());
-			}
-
-			for (String ligne : listeLignes) {
 
 				// on récupere les produits
 				Produit produit = new Produit(ligne);
 				listeProduits.add(produit);
+
+				System.out.println(ligne);
+
 			}
+
+			// // enregistrement dans la BDD du contenu de la liste de Categories
+			// CategorieDao catDao = new CategorieDao();
+			// Iterator<Categorie> monIt = listeCategories.iterator();
+			// while (monIt.hasNext()) {
+			// Categorie maCat = monIt.next();
+			// catDao.insert(maCat);
+			// // System.out.println(maCat.getNom());
+			// }
+			//
+			// // enregistrement dans la BDD du contenu de la liste de Marques
+			// MarqueDao marDao = new MarqueDao();
+			// Iterator<Marque> monIt2 = listeMarques.iterator();
+			// while (monIt2.hasNext()) {
+			// Marque maMar = monIt2.next();
+			// marDao.insert(maMar);
+			//
+			// // System.out.println(maMar.getNom());
+			// }
+			//
 
 			// recupération du driver sql pour la connection à la bdd
 			try {
@@ -118,8 +119,9 @@ public class App {
 		long end = System.currentTimeMillis();
 		long dureeTraitementApp = (end - start) / 1000;
 		timer.cancel();
-		System.out.println("------------ FIN du traitement ------------");
+		System.out.println("------------ FIN du traitement du fichier  ------------");
 		System.out.println("duree du traitement : " + dureeTraitementApp + " secondes");
+
 	}
 
 }
--- src/main/java/fr/diginamic/traitement_fichier/model/Produit.java
+++ src/main/java/fr/diginamic/traitement_fichier/model/Produit.java
@@ -18,7 +18,30 @@ public class Produit {
 	private List<Ingredient> listeIngredients;
 	private List<Allergene> listeAllergenes;
 
-	/** presenceHuilePalme : Integer 1=oui, 0=non communiqué, -1=non */
+	private String energie100g;
+	private String graisse100g;
+	private String sucres100g;
+	private String fibres100g;
+	private String proteines100g;
+	private String sel100g;
+	private String vitA100g;
+	private String vitD100g;
+	private String vitE100g;
+	private String vitK100g;
+	private String vitC100g;
+	private String vitB1100g;
+	private String vitB2100g;
+	private String vitPP100g;
+	private String vitB6100g;
+	private String vitB9100g;
+	private String vitB12100g;
+	private String calcium100g;
+	private String magnesium100g;
+	private String iron100g;
+	private String fer100g;
+	private String betaCarotene100g;
+
+	// presenceHuilePalme : Integer 1=oui, 0=non communiqué, -1=non
 	private Integer presenceHuilePalme;
 
 	public Produit(String ligne) {
@@ -33,6 +56,35 @@ public class Produit {
 
 		this.nom = attributs[2];
 		this.nutrigrade = attributs[2];
+
+		this.energie100g = attributs[5];
+		this.graisse100g = attributs[6];
+		this.sucres100g = attributs[7];
+
+		this.fibres100g = attributs[8];
+		this.proteines100g = attributs[9];
+		this.sel100g = attributs[10];
+
+		this.vitA100g = attributs[11];
+		this.vitD100g = attributs[12];
+		this.vitE100g = attributs[13];
+
+		this.vitK100g = attributs[14];
+		this.vitC100g = attributs[15];
+		this.vitB1100g = attributs[16];
+
+		this.vitB2100g = attributs[17];
+		this.vitPP100g = attributs[18];
+		this.vitB6100g = attributs[19];
+
+		this.vitB9100g = attributs[20];
+		this.vitB12100g = attributs[21];
+		this.calcium100g = attributs[22];
+
+		this.magnesium100g = attributs[23];
+		this.iron100g = attributs[24];
+		this.fer100g = attributs[25];
+		this.betaCarotene100g = attributs[26];
 
 		if (attributs[27] == "1") {
 			this.presenceHuilePalme = 1;
